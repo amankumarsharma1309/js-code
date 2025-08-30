5ca246edfe796a8691cd4e8ff5dabbacea498062
amankumarsharma1309 <146988180+amankumarsharma1309@users.noreply.github.com>
2025-08-30 01:50:19 +0000

    03_conversionOperations

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId= 345552
+var accountPassword= "12345"
+let acocuntEmail= "aman@google.com"
+accountCity="kkr"
+
+//accountId =2
+//console.log(accountId);
+accountPassword= "4567"
+acocuntEmail="kumar@google.com"
+accountCity="bhutan"
+/*
+prefer not to use var 
+cuz of the issue in teh functional scope and block scope 
+*/
+
+console.table([accountPassword, accountCity, acocuntEmail])
+
+
+
+
---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,11 @@
+let score= "33"
+console.log(typeof score);
+console.log(typeof (score))
+
+let valueInNumber= Number(score)
+
+console.log(typeof valueInNumber);
+
+//33=> 33
+//"33abc"=>NaN 
+// true=> 1; or false 0;
