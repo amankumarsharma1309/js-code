ac33fa310279d56b90b71facf4b2b9cb4953e1a4
amankumarsharma1309 <146988180+amankumarsharma1309@users.noreply.github.com>
2025-09-03 05:14:33 +0000

    filter

---
+++ 05_loop/filterMapAndReduce.js
@@ -0,0 +1,13 @@
+//const coding =["js", "ruby", "java", "cpp"]
+
+
+// const values= coding.forEach( (item)=>{
+//     //console.log(item)
+//     return item
+// })
+
+// console.log(values)
+
+//as seen above that this for-each loop cant return values 
+
+
